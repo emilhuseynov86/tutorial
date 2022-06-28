7b38fd49abe50cbff171b0fbfa2f10bbaf5413c0
Emil Huseynov <emilhuseynov86@gmail.com>
2022-06-28 14:29:09 +0400

    intro

---
+++ php-beginner/intro/comments.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 'Hello, world'; // bu kod Hello, world ifadəsini ekrana çıxarır
+
+echo '<br>';
+
+echo 5 * 2; # bu kod 5 və 2 ədədlərinin hasilini ekrana çıxarır
+
+/* Aşağıdakı kod 5-in 2-yə qalıqlı bölməsini hesablayır.
+   Bildiyimiz kimi 5 ədədi 2-yə tam bölünmür, qalıq 1-ə
+   bərabərdir.
+ */
+
+echo '<br>';
+echo 5 % 2;
