c7f7a6392ab5aec6b6c83f7c776a9930c302b870
Emil Huseynov <emilhuseynov86@gmail.com>
2022-09-27 07:36:10 +0400

    operators

---
+++ php-beginner/operators/arithmetic.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(-1);
+
+$x = 5;
+$y = 3;
+
+var_dump($x + $y);
+var_dump($x - $y);
+var_dump($x * $y);
+var_dump($x / $y);
+var_dump($x % $y);
+var_dump($x ** $y);
---
+++ php-beginner/operators/modulo-operator.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting(-1);
+
+$a = 5.5;
+$b = 3.9;
+
+echo $a % $b;
